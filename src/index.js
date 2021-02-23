f92d391c7b3ed42603e019ab6db1123f497b0e6f
Anna <61065956+AnnaIgnatova@users.noreply.github.com>
2021-02-23 12:07:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,87 @@
 module.exports = function toReadable (number) {
-  
+  numbers = [{1 : 'one'}, {2 : 'two'}, {3 : 'three'}, {4 : 'four'}, {5 : 'five'}, {6 : 'six'}, {7 : 'seven'}, {8 : 'eight'}, {9 : 'nine'}, {10: 'ten'}, {11: 'eleven'}, {12: 'twelve'}, {13: 'thirteen'}, {14: 'fourteen'}, {15: 'fifteen'}, {16: 'sixteen'}, {17: 'seventeen'}, {18: 'eighteen'}, {19: 'nineteen'}] ;
+  decimals = [{2: 'twenty'}, {3: 'thirty'}, {4: 'forty'}, {5: 'fifty'}, {6: 'sixty'}, {7: 'seventy'}, {8: 'eighty'}, {9: 'ninety'}];
+  str ='';
+  if (number == 0) return 'zero';
+  while (number) {
+    if (number > 0 && number <= 19) {
+      numbers.forEach( item => {
+        for (key in item) {
+          if (key == number) str = item[key];
+        }
+      });
+      break;
+    } 
+    if (number >= 20 && number <= 99) {
+      last = number % 10;
+      if (last == 0) str = ''; 
+      else {
+        numbers.forEach(item => {
+          for (key in item) {
+            if (key == last) str = ' ' + item[key];
+          }
+        }); 
+      }
+      number = Math.floor(number/10);
+      last = number % 10;
+      decimals.forEach(item => {
+        for (key in item) {
+          if (key == last) str = item[key] + str;
+        }
+      });
+      break;
+   
+    }
+    if (number >= 100 && number <= 999) {
+      if (number % 100 == 0) {
+        last = Math.floor(number / 100);
+        numbers.forEach(item => {
+          for (key in item) {
+            if (key == last) str = item[key] + ' hundred'; 
+          }
+        });
+        break;
+      }
+      last = number % 100;
+      if (last > 0 && last <= 19) {
+        numbers.forEach( item => {
+          for (key in item) {
+            if (key == last) str = item[key];
+          }
+        });
+        number = Math.floor(number/100);
+      }
+      else {
+        last = number % 10;
+        if (last == 0) str = ''; 
+        else {
+          numbers.forEach(item => {
+            for (key in item) {
+              if (key == last) str = ' ' + item[key];
+            }
+          }); 
+        }
+        number = Math.floor(number/10);
+        last = number % 10;
+        if (last == 0) str = str + '';
+        else {
+          decimals.forEach(item => {
+            for (key in item) {
+              if (key == last) str = item[key] + str;
+            }
+          });
+        }
+        number = Math.floor(number/10);
+      }
+      last = number % 10;
+      numbers.forEach(item => {
+        for (key in item) {
+          if (key == last) str = item[key] + ' hundred ' + str;
+        }
+      });
+      break;
+   
+    }
+  }
+  return str;
 }
